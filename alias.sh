9e435ddb2c250760af44dca8684328bfcd241d44
sbkoth <sbkoth@users.noreply.github.com>
2017-03-25 13:12:45 +0530

    Create alias.sh

---
+++ alias.sh
@@ -0,0 +1,11 @@
+## A collection of useful two letter aliases
+
+alias ll="ls -alht"
+alias ga="git add *"
+alias gc="git commit -a"
+alias gp="git push"
+alias gs="git status"
+alias gl="git pull"
+alias bu="brew upgrade"
+alias bc="brew cleanup"
+alias bp="brew prune"
