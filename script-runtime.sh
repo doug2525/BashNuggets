dd244ad53fac3413604870d3540d836ca091121d
sbkoth <sbkoth@users.noreply.github.com>
2017-03-25 12:36:02 +0530

    Create script-runtime.sh

---
+++ script-runtime.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+START=$(date +%s)
+######### Put your script between #########
+
+######### these comment lines #########
+END=$(date +%s)
+DIFF=$(( $END - $START ))
+DIFF=$(( $DIFF / 60 ))
+echo "The script took $DIFF minutes to run"
