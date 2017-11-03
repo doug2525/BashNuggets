36074c735e66ec502743fda47997880730d55cc5
sbkoth <sbkoth@users.noreply.github.com>
2017-11-03 18:19:52 -0400

    Update start-all-docker-containers.sh

--- start-all-docker-containers.sh
+++ start-all-docker-containers.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-# This script start all stopped containers on a given machine. May need to be run multiple times if there are dependent containers like Kubernetes. 
+# This script start all stopped containers on a given machine. 
+# May need to be run multiple times if there are dependent containers like Kubernetes. 
 
 docker start $(docker ps -a | awk '{print $1 }' | tail -n +2 | tr '\n' " ")
