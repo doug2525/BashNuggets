a8225f6015a57949af13028432b703401fc0b777
sbkoth <skothab@gmail.com>
2017-11-12 16:02:23 -0500

    Add command to swap arbitrary columns.

--- columnswap.sh
+++ columnswap.sh
@@ -1,4 +1,8 @@
-#!/bin/env bash
+#!/bin/bash
 # This script flips the first two columns in a given file.
 
-awk ' { t = $1; $1 = $2; $2 = t; print; }' input_file
+awk ' { t = $i; $i = $j; $j = t; print; }' input_file
+
+# To swap specified columns in file... Change i and j for differet columns
+
+awk '{t=$i;$i=$j;$j=t;}1' i=2 j=3  input_file 
