251bb5798e41f3817a4f3a8d43a1a0a45cbd8975
sbkoth <sbkoth@users.noreply.github.com>
2017-12-19 12:44:12 -0500

    Update delete-dead-docker-containers.sh

--- delete-dead-docker-containers.sh
+++ delete-dead-docker-containers.sh
@@ -1,4 +1,4 @@
-#/bin/bash
+#!/bin/bash
 docker ps -a | grep Exited | awk '{print $1}' > inactive-containers
 while read -r line; do docker rm $line;echo "Deleted container $line"; done < inactive-containers 
 rm -f inactive-containers
