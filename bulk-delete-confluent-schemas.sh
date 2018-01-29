17e44e5bb19300dd8568261304784b2cbc06e327
Doug Troxel <doug.a.troxel@monsanto.com>
2018-01-29 15:25:32 -0600

    Update bulk-delete-confluent-schemas.sh
    added code to pass the environments as a second parameter

--- bulk-delete-confluent-schemas.sh
+++ bulk-delete-confluent-schemas.sh
@@ -2,8 +2,21 @@
 ### This simple script deletes all the subjects provided one per line in a text file. See Confluent's Schema Registry API Docs here: https://docs.confluent.io/current/schema-registry/docs/api.html
 ### Usage: chmod +x bulk-delete-confluent-schemas.sh; ./bulk-delete-confluent-schemas.sh subjects.txt
 ### Schemas will be permanently deleted. Use with caution.
+line=$1
+sr=$2
+
+case $sr in
+
+	dev) address=
+	;;
+	np) address=
+	;;
+	prod) address=
+	;;
+esac
+
 while IFS='' read -r line || [[ -n "$line" ]]; do
-    echo -e "Deleting subject $line"
-    curl -XDELETE <SR-URL>/subjects/$line
+    echo -e "Deleting subject $line in $sr environment."
+  curl -XDELETE $address/subjects/$line
     echo -e "\n"
 done < "$1"
