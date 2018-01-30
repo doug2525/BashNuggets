e49d9eb197f54aa86193b92f882c8d5917c19d8a
Doug Troxel <doug.a.troxel@monsanto.com>
2018-01-30 09:47:25 -0600

    Update bulk-delete-confluent-schemas.sh
    Added prompt for environments instead of having to add it as a parameter.

--- bulk-delete-confluent-schemas.sh
+++ bulk-delete-confluent-schemas.sh
@@ -1,22 +1,27 @@
 #!/bin/bash
 ### This simple script deletes all the subjects provided one per line in a text file. See Confluent's Schema Registry API Docs here: https://docs.confluent.io/current/schema-registry/docs/api.html
-### Usage: chmod +x bulk-delete-confluent-schemas.sh; ./bulk-delete-confluent-schemas.sh subjects.txt (dev/np/prod)
+### Update the script with each environment's schema registry api address in the address= variables
+### Usage: chmod +x bulk-delete-confluent-schemas.sh; ./bulk-delete-confluent-schemas.sh subjects.txt where subjects.txt is the 
+### 	   list of schema subjects to be deleted.
 ### Schemas will be permanently deleted. Use with caution.
 line=$1
-sr=$2
 
-case $sr in
+read -rp $'What environment?: \n1) Dev \n2) Non-Prod \n3) Prod\n' i
+case $i in
 
-	dev) address=
+	1) address=
+	   env=Dev	
 	;;
-	np) address=
+	2) address=
+	   env=Non=Prod
 	;;
-	prod) address=
+	3) address=
+	   env=Prod
 	;;
 esac
 
 while IFS='' read -r line || [[ -n "$line" ]]; do
-    echo -e "Deleting subject $line in $sr environment."
+    echo -e "Deleting subject $line in $env."
   curl -XDELETE $address/subjects/$line
     echo -e "\n"
 done < "$1"
