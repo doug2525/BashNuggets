2e788f2d98e38865f6717baeb01139d934126e04
Doug Troxel <doug.a.troxel@monsanto.com>
2018-02-21 11:49:36 -0600

    Update bulk-delete-confluent-schemas.sh
    Added ability to remove certain versions of the schema .

--- bulk-delete-confluent-schemas.sh
+++ bulk-delete-confluent-schemas.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-### This simple script deletes all the subjects provided one per line in a text file. See Confluent's Schema Registry API Docs here: https://docs.confluent.io/current/schema-registry/docs/api.html
+### This simple script deletes all the subjects/versions provided one per line in a text file.
+### To remove the schema an all versions just put the schema name in the text file.
+### To remove only certain versions of the schema include the name and the versions you want to remove seperated by commas.
+###  i.e. my.schema.name.json-value,1,2,3
+### See Confluent's Schema Registry API Docs here: https://docs.confluent.io/current/schema-registry/docs/api.html
 ### Update the script with each environment's schema registry api address in the address= variables
 ### Usage: chmod +x bulk-delete-confluent-schemas.sh; ./bulk-delete-confluent-schemas.sh subjects.txt where subjects.txt is the 
 ### 	   list of schema subjects to be deleted.
@@ -9,19 +13,30 @@ line=$1
 read -rp $'What environment?: \n1) Dev \n2) Non-Prod \n3) Prod\n' i
 case $i in
 
-	1) address=
-	   env=Dev	
-	;;
-	2) address=
-	   env=Non=Prod
-	;;
-	3) address=
-	   env=Prod
-	;;
+        1) address=http://kafka-sr-dev.monsanto.com:8086
+           env=Dev
+        ;;
+        2) address=http://kafka-np-sr.monsanto.com:8081
+           env=Non=Prod
+        ;;
+        3) address=http://kafka-sr.monsanto.com:8081
+           env=Prod
+        ;;
 esac
 
 while IFS='' read -r line || [[ -n "$line" ]]; do
-    echo -e "Deleting subject $line in $env."
-  curl -XDELETE $address/subjects/$line
+    name="$( cut -d ',' -f 1 <<< "$line" )";
+    version="$( cut -d ',' -f 2- <<< "$line" )";
+
+    if [ $name==$version ]; then
+        echo -e "Deleting subject $name in $env."
+        curl -XDELETE $address/subjects/$name;
+    else
+        for i in $(echo $version | sed "s/,/ /g")
+        do
+                echo -e "Deleting subject $name -  version $i in $env."
+                curl -XDELETE $address/subjects/$name/versions/$i;
+        done
+    fi
     echo -e "\n"
 done < "$1"
